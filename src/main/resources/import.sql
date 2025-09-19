15c2a274d3da0c22acc1ebe871c53d9dc6dc3712
Fernando Rosendo <darwinism9230@gmail.com>
2025-09-18 23:07:22 -0300

    insere valores na tabela de produto ao iniciar

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+-- Produtos de empréstimo para testes
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Emprestimo Pessoal', 18.0, 24);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Financiamento Veiculo', 12.0, 72);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Credito Consignado', 8.5, 84);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Financiamento Imobiliario', 9.2, 420);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Capital de Giro', 22.0, 36);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Credito Rural', 6.5, 120);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Microcredito', 15.0, 24);
+INSERT INTO produto (nome, taxa_juros_anual, prazo_maximo_meses) VALUES ('Antecipacao Recebiveis', 25.5, 12);
