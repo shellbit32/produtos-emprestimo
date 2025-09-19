95444b553c3878287e544477f32a5227e7c651f1
Fernando Rosendo <darwinism9230@gmail.com>
2025-09-18 22:08:28 -0300

    cria resource de simulacao

--- src/main/java/rest/resource/SimulacaoResource.java
+++ src/main/java/rest/resource/SimulacaoResource.java
@@ -1,4 +1,32 @@
 package rest.resource;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import repository.ProdutoRepository;
+import rest.dto.RequestSimulacaoEmprestimoDTO;
+
+@Path("/simulacoes")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class SimulacaoResource {
+
+    private ProdutoRepository produtoRepository;
+
+    @Inject
+    public SimulacaoResource(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+    }
+
+    @GET
+    public Response simularEmprestimo(RequestSimulacaoEmprestimoDTO request){
+        /*
+        Realizar cálculo de empréstimo e retornar um ResponseSimulacaoEmprestimoDTO já montado
+         */
+        return Response.ok(request).build();
+    }
 }
