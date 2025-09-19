f329fd01d9a43fe2fbe2143c1bf88c979c32337d
Fernando Rosendo <darwinism9230@gmail.com>
2025-09-18 22:07:41 -0300

    cria DTO de request de simulacao de emprestimo

--- src/main/java/rest/dto/RequestSimulacaoEmprestimoDTO.java
+++ src/main/java/rest/dto/RequestSimulacaoEmprestimoDTO.java
@@ -1,4 +1,12 @@
 package rest.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class RequestSimulacaoEmprestimoDTO {
+    private Long idProduto;
+    private BigDecimal valorSolicitado;
+    private Integer prazoMeses;
 }
