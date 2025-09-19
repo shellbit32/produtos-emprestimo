5ec8923a7db33605c4fce116c596b938f40ac9c6
Fernando Rosendo <darwinism9230@gmail.com>
2025-09-18 22:06:23 -0300

    cria DTO de Response para simulacao de emprestimo

--- src/main/java/rest/dto/ParcelaSimulacaoDTO.java
+++ src/main/java/rest/dto/ParcelaSimulacaoDTO.java
@@ -1,4 +1,14 @@
 package rest.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ParcelaSimulacaoDTO {
+    private Integer mes;
+    private BigDecimal saldoDevedorInicial;
+    private BigDecimal juros;
+    private BigDecimal amortizacao;
+    private BigDecimal saldoDevedorFinal;
 }
--- src/main/java/rest/dto/ResponseSimulacaoEmprestimoDTO.java
+++ src/main/java/rest/dto/ResponseSimulacaoEmprestimoDTO.java
@@ -1,4 +1,17 @@
 package rest.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
 public class ResponseSimulacaoEmprestimoDTO {
+    private ProdutoDTO produto;
+    private BigDecimal valorSolicitado;
+    private Integer prazoMeses;
+    private BigDecimal taxaJurosEfetivaMensal;
+    private BigDecimal valorTotalComJuros;
+    private BigDecimal parcelaMensal;
+    private List<ParcelaSimulacaoDTO> memoriaCalculo;
 }
