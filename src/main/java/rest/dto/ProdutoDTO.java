f3193494e4d8158ae59ae939c7de973ca177f981
Fernando Rosendo <darwinism9230@gmail.com>
2025-09-18 22:07:16 -0300

    cria DTO de produto

--- src/main/java/rest/dto/ProdutoDTO.java
+++ src/main/java/rest/dto/ProdutoDTO.java
@@ -1,4 +1,11 @@
 package rest.dto;
 
+import lombok.Data;
+
+@Data
 public class ProdutoDTO {
+    private Long id;
+    private String nome;
+    private Double taxaJurosAnual;
+    private Integer prazoMaximoMeses;
 }
